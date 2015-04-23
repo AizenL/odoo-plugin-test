660ea589a0f07b990a95ce4291c7cf421b39fec9
Muhammad Fariz Arya <muhammadfarizarya@gmail.com>
2015-04-23 10:17:51 +0700

    first test

---
+++ training 1/newmodule/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'New Module Tutorial',
+    'version': '1.0',
+    'author': 'Fariz',
+    'complexity': 'normal',
+    'description': 'Another new module for tutorial',
+    'category': '',
+    'depends': [],
+    'data': [
+    	'views/mahasiswa_view.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+    },
+}
